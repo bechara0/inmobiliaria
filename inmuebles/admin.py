a1cb93b135f8981b5ad28e99c7eab564793a6895
bechara0 <jjbechara@gmail.com>
2023-05-24 17:22:17 -0300

    Ya se creo la base de datos, todo funciona

--- inmuebles/admin.py
+++ inmuebles/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Alquiler, Ventas, Tipo, Estado, Localidad, Moneda
 
-# Register your models here.
+
+class LocalidadAdmin(admin.ModelAdmin):
+    list_display = ("localidad",)
+
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ("estado",)
+
+
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ("tipo_propiedad",)
+
+
+class VentasAdmin(admin.ModelAdmin):
+    list_display = ("direccion", "lugar", "tipo")
+    list_filter = ("lugar", "tipo", "fecha_publicacion")
+    search_fields = ("lugar__localidad", "direccion", "tipo__nombre")
+
+
+class AlquilerAdmin(admin.ModelAdmin):
+    list_display = ("direccion", "lugar", "tipo", "disponibilidad")
+    list_filter = ("lugar", "tipo", "fecha_publicacion", "disponibilidad")
+    search_fields = ("lugar__localidad", "direccion", "tipo__nombre")
+
+
+admin.site.register(Ventas, VentasAdmin)
+admin.site.register(Alquiler, AlquilerAdmin)
+admin.site.register(Tipo, TipoAdmin)
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Localidad, LocalidadAdmin)
+admin.site.register(Moneda)
