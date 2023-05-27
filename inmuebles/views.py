7104b7bfd31003d8a89df734f45ad697c9f95dab
bechara0 <jjbechara@gmail.com>
2023-05-27 17:07:39 -0300

    lalala

--- inmuebles/views.py
+++ inmuebles/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from .models import Inmueble
 
 
 def alquiler(request):
-    return render(request, "inmuebles/alquiler.html")
+    alquileres = Inmueble.objects.filter(disponibilidad__in=[2, 3])
+    context = {
+        "alquileres": alquileres,
+    }
+    return render(request, "inmuebles/alquiler.html", context)
 
 
 def venta(request):
-    return render(request, "inmuebles/venta.html")
+    ventas = Inmueble.objects.filter(disponibilidad__in=[1, 3])
+    context = {
+        "ventas": ventas,
+    }
+    return render(request, "inmuebles/venta.html", context)
+
+
+def todos(request):
+    totalidad = Inmueble.objects.all()
+    context = {
+        "totalidad": totalidad,
+    }
+    return render(request, "inmuebles/todos.html", context)
